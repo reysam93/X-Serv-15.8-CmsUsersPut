494c14073d09111972f777d8edc38412004e8aff
Samuel <samuel@samuel.(none)>
2015-04-17 18:55:54 +0200

    varios put en un mismo recurso lo actualzan no duplican

---
+++ cms_users_put/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
+from models import Page
+
+
+def processCmsRequest(request, resource):
+    if request.user.is_authenticated():
+        loged = "<br><br>Logged in as " + request.user.username +\
+                ". <a href='/admin/logout/'>Logout</a><br>"
+    else:
+        loged = "<br><br>Not logged. <a href='/admin/login/'>Login</a><br>"
+    if request.method == 'GET':
+        try:
+            page = Page.objects.get(name=resource)
+            return HttpResponse(page.page + loged)
+        except Page.DoesNotExist:
+            return HttpResponseNotFound("Page not found" + loged)
+    elif request.method == 'PUT':
+        if request.user.is_authenticated():
+            newPage = Page(name=resource, page=request.body)
+            newPage.save()
+            return HttpResponse("Added to the list")
+        else:
+            return HttpResponse("Couldn't add to the list" + loged)
+    else:
+        return HttpResponse(status=403)
